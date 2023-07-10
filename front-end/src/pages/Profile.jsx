94983340e4c62972f68ac855a739e1a6bf75bc68
Joseph Archer <viewtiful.kja@gmail.com>
2023-07-10 10:44:28 -0400

    profile

--- front-end/src/pages/Profile.jsx
+++ front-end/src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import { AuthContext } from '../utils/AuthContext';
 import useAxios from "../utils/useAxios";
 
 const Profile = () => {
-
+    const [isButtonClicked, setButtonClicked] = useState(false);
     const { id } = useParams();
 
     const { data, loading, error } = useAxios(`/api/users/${id}`);
@@ -17,35 +17,49 @@ const Profile = () => {
         return <div>Error: {error.message}</div>;
     }
 
+    const handleButtonClick = () => {
+        setButtonClicked(true);
+        setTimeout(() => {
+            setButtonClicked(false);
+        }, 2000);
+    };
+
     return (
-        <div className="bg-gray-100 max-h-screen">
-            <div className="max-w-3xl mx-auto px-4 py-8">
-                <div className="bg-white shadow-md rounded-lg overflow-hidden">
-                    <div className="flex">
-                        <div className="w-1/2">
-                            <img
-                                src="http://via.placeholder.com/640x360"
-                                alt="Profile"
-                                className="object-cover w-full"
-                            />
+        <div className="container mx-auto py-8">
+            <div className="bg-white rounded-lg shadow-lg">
+                <div className="flex justify-between items-center px-8 py-6 border-b">
+                    <h1 className="text-2xl font-bold">Profile</h1>
+                    <button
+                        className={`bg-red-600 hover:bg-red-800 text-white font-bold px-4 py-2 rounded ${isButtonClicked ? 'cursor-default' : 'cursor-pointer'
+                            }`}
+                        onClick={handleButtonClick}
+                        disabled={isButtonClicked}
+                    >
+                        {isButtonClicked ? 'Coming soon...' : 'Edit Profile'}
+                    </button>
+                </div>
+                <div className="flex flex-col md:flex-row p-8">
+                    <div className="w-full md:w-1/4">
+                        <img src={data.profileImage} alt="Profile" className="w-full rounded-lg" />
+                    </div>
+                    <div className="w-full md:w-3/4 md:pl-8 mt-8 md:mt-0">
+                        <h2 className="text-xl font-bold mb-4">{data.firstName} {data.lastName}</h2>
+                        {/* <p className="text-gray-600">Software Developer</p> */}
+                        <div className="flex items-center mt-4">
+                            {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M10 2a8 8 0 100 16 8 8 0 000-16zM5.5 10a.5.5 0 000 1h9a.5.5 0 000-1h-9z" clipRule="evenodd" />
+                            </svg>
+                            <span className="text-gray-600">San Francisco, CA</span> */}
                         </div>
-                        <div className="w-1/2 px-6 py-8">
-                            <h2 className="text-3xl font-bold mb-4">{data.firstName} {data.lastName}</h2>
-                            <p className="text-gray-600 text-lg mb-4">
-                                Email:
-                            </p>
-                            {/* <p className="text-gray-600 text-lg mb-4">
-                                Team: {profile.team}
-                            </p>
-                            <p className="text-gray-600 text-lg mb-4">
-                                Number: {profile.number}
-                            </p>
-                            <p className="text-gray-600 text-lg mb-4">
-                                Height: {profile.height}
-                            </p>
-                            <p className="text-gray-600 text-lg mb-4">
-                                Weight: {profile.weight}
-                            </p> */}
+                        <div className="flex items-center mt-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-gray-400 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                <path fillRule="evenodd" d="M10 2a8 8 0 100 16 8 8 0 000-16zM5.5 10a.5.5 0 000 1h9a.5.5 0 000-1h-9z" clipRule="evenodd" />
+                            </svg>
+                            <span className="text-gray-600">{data.email}</span>
+                        </div>
+                        <div className="mt-8">
+                            <h3 className="text-lg font-semibold mb-4">About Me</h3>
+                            <p className="text-gray-600">...</p>
                         </div>
                     </div>
                 </div>
