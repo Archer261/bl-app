f6662d5257b0723e69377a63bd9605af5a811019
Joseph Archer <viewtiful.kja@gmail.com>
2023-07-10 08:49:45 -0400

    few more changes to the navigation

--- front-end/src/components/Hero.jsx
+++ front-end/src/components/Hero.jsx
@@ -6,58 +6,28 @@ import DateFormatted from '../utils/DateFormatted';
 const Hero = ({ challenge }) => {
 
     return (
-        <section className="overflow-hidden relative sm:grid sm:grid-cols-2 rounded-lg">
-            <div className="p-8 md:p-12 lg:px-16 lg:py-24 relative z-10 flex flex-col justify-center">
-                <div className="max-w-xl mx-auto text-center ltr:sm:text-left rtl:sm:text-right">
-                    <h2 className="text-2xl font-bold text-gray-900 md:text-3xl">
-                        {challenge.name}
-                    </h2>
-
-                    <div className="text-gray-800 font-semibold mt-4 md:block">
-
-                        Participant(s): {challenge.participants.length}
-                    </div>
-
-                    <div className="text-gray-800 font-semibold mt-4 md:block">
-
-                        <DateFormatted textValue={'Start Date: '} dateValue={challenge.startDate} />
-                    </div>
-                    <div className="text-gray-800 font-semibold mt-4 md:block">
-
-                        <DateFormatted textValue={'End Date: '} dateValue={challenge.endDate} />
-                    </div>
-                    <p className="text-gray-800 font-semibold mt-4 md:block">
-                        {challenge.weekCount} Weeks
-                    </p>
-
-                    <div className="card border w-96 hover:shadow-none relative flex flex-col mx-auto shadow-lg m-5">
-                        <img
-                            src={logo}
-                            alt="Avatar"
-                            className='w-28 h-28 p-1 bg-white rounded-full mx-auto mb-4'
-                        />
-                        <h5 className="title mt-11 ml-3 font-bold flex flex-col">Foundry</h5>
-                        <p className="text-neutral-900 dark:text-neutral-500">Organizer</p>
-                    </div>
-
-                    <div className="mt-4 md:mt-8">
-                        <Link to={`/challenge/${challenge._id}`}>
-                            <button className="inline-block rounded bg-emerald-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-emerald-700 focus:outline-none focus:ring focus:ring-yellow-400">
-                                Open
-                            </button>
-                        </Link>
+        <>
+            <div className="mx-auto max-w-lg text-center">
+                <h2 className="text-3xl font-bold sm:text-4xl">Featured Challenge</h2>
+                <p className="my-4">View the leaderboard!</p>
+            </div>
+            <div className="card lg:card-side bg-glass shadow-xl">
+                <figure><img
+                    src={base}
+                    alt="Album"
+                    className='px-2 h-auto w-96'
+                /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">{challenge.name}</h2>
+                    <DateFormatted textValue={'Start Date: '} dateValue={challenge.startDate} />
+                    <DateFormatted textValue={'End Date: '} dateValue={challenge.endDate} />
+                    <p>{challenge.weekCount} Weeks</p>
+                    <div className="card-actions justify-center">
+                        <Link to={`/challenge/${challenge._id}`}><button className="btn btn-secondary">View</button></Link>
                     </div>
                 </div>
             </div>
-            <div className="absolute inset-0 bg-gradient-to-b from-white to-transparent opacity-50"></div>
-            <div className="flex justify-center items-center p-8">
-                <img
-                    alt="challenge"
-                    src={base}
-                    className="h-auto object-contain sm:h-auto max-w-full max-h-96"
-                />
-            </div>
-        </section>
+        </>
     );
 };
 
--- front-end/src/pages/Challenges.jsx
+++ front-end/src/pages/Challenges.jsx
@@ -55,44 +55,14 @@ const Challenges = () => {
                                         {/* Organizer: {c.organizer} */}
                                     </p>
                                 </div>
-                                {/* <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    className="h-10 w-10 text-pink-500"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke="currentColor"
-                                >
-                                    <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                                    <path
-                                        d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                                    />
-                                    <path
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        strokeWidth="2"
-                                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                                    />
-                                </svg>
-
-                                <h2 className="mt-4 text-xl font-bold">{c.name}</h2>
-
-                                <div className="mt-1 text-sm">
-                                    <DateFormatted dateValue={c.startDate} textValue={'Start Date: '} />
-                                    <DateFormatted dateValue={c.endDate} textValue={'End Date: '} />
-                                </div> */}
                             </Link>
                         </motion.div>
                     ))}
                 </div>
                 {user && (
                     user.isAdmin && (
-                        <div className="mt-12 text-center">
-                            <Link
-                                to={'/create-challenge'}
-                                className="inline-block rounded bg-pink-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-pink-700 focus:outline-none focus:ring focus:ring-yellow-400"
-                            >
-                                New Challenge
-                            </Link>
+                        <div className="card-actions justify-center">
+                            <Link to={`/create-challenge`}><button className="btn btn-secondary">Create New Challenge</button></Link>
                         </div>
                     )
                 )}
