278d64f585375e10c005590601e2b322a852b0f4
Joseph Archer <viewtiful.kja@gmail.com>
2023-07-12 17:38:09 -0400

    more layout updates

--- front-end/src/components/PotCount.jsx
+++ front-end/src/components/PotCount.jsx
@@ -4,9 +4,9 @@ import { pot } from '../assets';
 
 const PotCount = (props) => {
     return (
-        <div className='flex-1 relative flex-col items-center justify-center max-w-60 min-h-auto text-yellow-300 p-0'>
+        <div className='flex-1 relative flex-col items-center justify-center max-w-72 min-h-auto text-yellow-300 p-0'>
             <img src={pot} className='max-w-full min-w-full' alt="prize-pot" />
-            <h1 className='absolute left-30 text-xl'>$<CountUp end={props.prize} /></h1>
+            <h1 className='absolute left-20 top-8 text-xl'>$<CountUp end={props.prize} /></h1>
         </div>
     )
 }
