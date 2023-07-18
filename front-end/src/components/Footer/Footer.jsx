dbee18ba126daf6bda0b5cec26aaae46e79a7e1a
Joseph Archer <104469447+Archer261@users.noreply.github.com>
2023-07-18 11:29:05 -0400

    Update Footer.jsx
    Footer changes

--- front-end/src/components/Footer/Footer.jsx
+++ front-end/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <footer className="py-8 px-4">
             <div className="flex flex-wrap justify-center md:justify-between space-y-4 md:space-y-0 md:flex-nowrap md:space-x-6">
-
+KJ Archer co.
             </div>
             <div className="mt-8 text-center">
                 <a href="#" className="text-gray-600 hover:text-gray-800">Privacy Policy</a>
